d2b516b3d36c7787c1df621d57b1160b3feede7c
AD001\z004efdb <shreya.malviya@siemens.com>
2022-08-10 09:27:53 +0530

    [Shreya] Add: UC1 for Emp Present or absent

---
+++ EmployeeWage/EmployeeWage/EmployeeAttendance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+   public class EmployeeAttendance
+    {
+        public void EmpAtt()
+        {
+            Console.WriteLine("This is UC1");
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            EmployeeAttendance employeeAttendance = new EmployeeAttendance();
+            employeeAttendance.EmpAtt();
+        }
+    }
+}
